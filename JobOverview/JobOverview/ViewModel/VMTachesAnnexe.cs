54e728774ae27ec0d544c53f9b4ab572f08e8106
ycappelle <ycappelle@YCAPPELLE17-DE>
2017-05-22 13:49:35 +0200

    yan annexe

--- JobOverview/JobOverview/ViewModel/VMTachesAnnexe.cs
+++ JobOverview/JobOverview/ViewModel/VMTachesAnnexe.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using JobOverview.Entity;
+using JobOverview.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,15 @@ namespace JobOverview.ViewModel
 {
     public class VMTachesAnnexe:ViewModelBase
     {
+        public List<Logiciel> Logiciels { get; set; }
+        public List<Personne> Personnes { get; set; }
+        public ObservableCollection<Tache> TachesAnnexes { get; set; }
+
+        public VMTachesAnnexe()
+        {
+            Logiciels = DALLogiciels.GetLogicielsVersions();
+            Personnes = DALPersonnes.GetPersonnesFromUser(Properties.Settings.Default.PersonneConnecte);
+            TachesAnnexes =new ObservableCollection < Tache >(DALTaches.GetTachesAnnexe());
+        }
     }
 }
